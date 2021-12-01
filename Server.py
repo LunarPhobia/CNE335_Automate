6ce110c9e50c31d2e3d0df43497aa960b8e2a35c
Nikita Chagay <nachagay@student.rtc.edu>
2021-11-30 17:36:03 -0800

    Paramiko SSH added
    SSH in with paramiko and run update/upgrade commands

--- Server.py
+++ Server.py
@@ -1,12 +1,27 @@
 import os
+import paramiko
 
 class Server:
     """ Server class for representing and manipulating servers. """
 
     def __init__(self, server_ip):
-        # TODO -
         self.server_ip = server_ip
 
     def ping(self):
-        # TODO - Use os module to ping the server
+        # Use os module to ping the server
         return os.system("ping " + self.server_ip)
+
+    def paramiko(self):
+        # creating SSH instance
+        ssh = paramiko.SSHClient()
+        ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+        ssh.connect(self.server_ip, 22, 'ec2-user')
+        # running commands
+        stdin, stdout, stderr = ssh.exec_command('sudo yum update')
+        for line in stdout.read().splitlines():
+            print(line)
+        stdin, stdout, stderr = ssh.exec_command('sudo yum upgrade')
+        for line in stdout.read().splitlines():
+            print(line)
+        # close SSH
+        ssh.close();
--- main.py
+++ main.py
@@ -5,14 +5,15 @@
 from Server import Server
 
 def print_program_info():
-    # TODO - Change your name
     print("Server Automator v0.1 by Nikita Chagay")
 
 # This is the entry point to our program
 if __name__ == '__main__':
     print_program_info()
-    # TODO - Create a Server object
     # Change IP address if needed
-    object = Server("3.145.30.238")
-    # TODO - Call Ping method and print the results
+    object = Server("13.58.74.211")
+    # Call Ping and paramiko methods and print the results
     print(Server.ping(object))
+    Server.paramiko(object)
+
+
